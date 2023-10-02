8404b7e647055c90fbf929efd7484e3462e1b6da
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-10-02 16:58:02 -0300

    feat: adding header

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import HeaderBar from './components/Header.vue';
+</script>
+
+<template>
+  <HeaderBar />
+</template>
+
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles/globals.css'
+
+createApp(App).mount('#app')
