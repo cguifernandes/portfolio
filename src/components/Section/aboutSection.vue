cba54c702f6aaa4bb9c415051fd0a57c0312ce15
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-10-12 00:09:17 -0300

    feat: adding animation

--- src/components/Section/aboutSection.vue
+++ src/components/Section/aboutSection.vue
@@ -1,13 +1,28 @@
 <template>
   <section class="h-[calc(100vh_-_80px)] bg-gradient-to-b relative flex items-center justify-between from-grey-900 to-grey-700">
     <div class="blur-top" />
-    <div class="text-white text-center flex flex-col text-2xl px-8 lg:text-left lg:text-2xl sm:text-3xl">
-      <p>
-        Meu nome é Guilherme Fernandes<br>
-        Eu desenvolvo coisas para a Internet<br>
-        Atualmente trabalho como <span class="text-blue-500">Desenvolvimento Web</span>
-      </p>
+    <div class="flex justify-between items-center w-full z-[5]">
+      <div class="text-white text-center flex flex-col text-2xl w-full px-6 lg:w-auto lg:text-left lg:text-xl xl:text-2xl lg:pl-20 xl:pl-24 2xl:pl-[125px]">
+        <p>
+          Meu nome é Guilherme Fernandes<br>
+          Eu desenvolvo coisas para a Internet<br>
+          Atualmente trabalho como <span class="text-blue-500">Desenvolvimento Web</span>
+        </p>
+      </div>
+      <div class="hidden lg:inline">
+        <Vue3Lottie
+          :animation-data="MainAnimation"
+          :loop="false"
+          class="lg:w-[460px] xl:w-[520px] 2xl:w-[680px] !m-0"
+        />
+      </div>
     </div>
     <div class="blur-bottom" />
   </section>
 </template>
+
+<script setup lang="ts">
+import { Vue3Lottie } from 'vue3-lottie'
+import MainAnimation from '../../assets/animation.json'
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { MotionPlugin } from '@vueuse/motion'
+import Vue3Lottie from 'vue3-lottie'
+import './styles/globals.css'
 
 const app = createApp(App)
 library.add(fab, fas)
 
 app.use(MotionPlugin)
+app.use(Vue3Lottie)
 app.component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
