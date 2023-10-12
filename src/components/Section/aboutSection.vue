7199f0e715c735766e5709d58fe823cc1579170a
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2023-10-11 22:29:57 -0300

    feat: finish about section for now

--- src/components/Section/aboutSection.vue
+++ src/components/Section/aboutSection.vue
@@ -1,12 +1,13 @@
 <template>
   <section class="h-[calc(100vh_-_80px)] bg-gradient-to-b relative flex items-center justify-between from-grey-900 to-grey-700">
-    <div class="absolute top-28 w-[1200px] right-24 h-12 bg-gradient-to-l from-blue-600 to-blue-300 blur-[150px] z-50" />
+    <div class="absolute top-12 w-[900px] right-24 h-12 bg-gradient-to-l hidden from-blue-600 to-blue-300 blur-[150px] z-50 md:inline" />
     <div class="text-white text-center flex flex-col text-2xl px-8 lg:text-left lg:text-2xl sm:text-3xl">
       <p>
         Meu nome é Guilherme Fernandes<br>
         Eu desenvolvo coisas para a Internet<br>
-        Trabalho com <span class="text-blue-500">Desenvolvimento Web</span>
+        Atualmente trabalho como <span class="text-blue-500">Desenvolvimento Web</span>
       </p>
     </div>
+    <div class="absolute md:bottom-6 min-w-[200px] max-w-[700px] w-full left-2/4 -translate-y-2/4 right-2/4 -translate-x-2/4 h-12 bg-gradient-to-l from-blue-600 to-blue-300 blur-[150px] z-50 md:left-12" />
   </section>
 </template>
