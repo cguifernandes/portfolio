1ac55fd826e96ca869f8215ec9e39a51a8391852
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2024-08-23 17:43:25 -0300

    feat: adjusting responsive

--- src/components/Section/historySection.vue
+++ src/components/Section/historySection.vue
@@ -24,38 +24,39 @@
       <div class="flex p-3 lg:pl-5 w-full flex-col gap-y-4 flex-1">
         <div class="flex flex-col max-w-2xl mx-auto lg:m-0 xl:max-w-4xl gap-y-4 pr-3">
           <div class="flex gap-x-4 items-center justify-center lg:justify-normal">
-            <img :src="MyImage" alt="My Image" class="xl:size-12 transition-all duration-200 border border-neutral-800 dark:border-neutral-200 size-10 object-cover rounded-full" />
+            <img :src="MyImage" alt="My Image" class="xl:size-12 transition-all duration-200 border border-neutral-800 dark:border-neutral-200 size-10 object-cover rounded-full hidden md:inline" />
             <h1 class="text-lg xl:text-xl font-montserrat text-neutral-800 dark:text-neutral-200  transition-all duration-200">{{ $t('Quem é ') }}<span class="gradient-text">Guilherme Fernandes?</span></h1>
           </div>
-          <p class="text-neutral-500 text-center lg:text-left text-sm xl:text-base">
-            {{ $t('text-about-me') }}
-            <br>
-            {{ $t('Para mais informações:') }} <a class="gradient-text" href="/curriculo.pdf">{{ $t('Currículo') }}</a>
-          </p>
+          <div class="flex flex-col">
+            <p class="text-neutral-500 text-center lg:text-left text-sm xl:text-base">
+              {{ $t('text-about-me') }}
+            </p>
+            <span class="text-neutral-500 text-center lg:text-left text-sm xl:text-base">{{ $t('Para mais informações:') }} <a class="gradient-text" href="/curriculo.pdf">{{ $t('Currículo') }}</a></span>
+          </div>
         </div>
         <div class="flex flex-col gap-y-2">
           <h1 class="text-lg xl:text-xl font-montserrat text-neutral-800 transition-all duration-300 dark:text-neutral-200">{{ $t('Serviços') }}</h1>
-          <div class="flex flex-wrap max-h-60 gap-3 overflow-x-auto pr-2 custom-scrollbar">
+          <div class="flex flex-wrap max-h-[103px] lg:max-h-[298px] gap-3 overflow-x-auto pr-2 custom-scrollbar">
             <SkillsCard
               :is-traslate="true"
               name="Desenvolvimento de site"
               class="!h-fit min-w-72"
               descripion="Crio websites personalizados que destacam sua presença online, utilizando as mais recentes tecnologias e design responsivo para sites atraentes, rápidos e fáceis de navegar. Transformo suas ideias em experiências digitais únicas para conquistar seus clientes."
-              description-class="line-clamp-4 md:line-clamp-5 xl:line-clamp-4"
+              description-class="line-clamp-3 md:line-clamp-5 xl:line-clamp-4"
             /> 
             <SkillsCard
               :is-traslate="true"
               name="Automação de tarefas"
               class="!h-fit min-w-72"
               descripion="Ofereço soluções de automação de tarefas para otimizar seu tempo e aumentar a produtividade, automatizando processos repetitivos e complexos. Assim, você pode focar nas atividades mais importantes e melhorar a eficiência operacional."
-              description-class="line-clamp-4 md:line-clamp-5 xl:line-clamp-4"
+              description-class="line-clamp-3 md:line-clamp-5 xl:line-clamp-4"
             />
             <SkillsCard
               :is-traslate="true"
               name="Design de sites"
               class="!h-fit min-w-72"
               descripion="Crio designs de sites atraentes e funcionais que refletem sua marca, com foco na experiência do usuário. Desenvolvo layouts intuitivos e impactantes que facilitam a navegação e engajam visitantes, transformando suas ideias em um design que destaca sua presença online."
-              description-class="line-clamp-4 md:line-clamp-5 xl:line-clamp-4"
+              description-class="line-clamp-3 md:line-clamp-5 xl:line-clamp-4"
             />
           </div>
         </div>
