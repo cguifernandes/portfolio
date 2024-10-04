7b604859689920e9eb6071a602d44dec4673ff56
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2024-10-04 18:16:09 -0300

    feat: add gtag

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { MotionPlugin } from '@vueuse/motion'
-import Vue3Lottie from 'vue3-lottie'
-import './styles/globals.css'
-import { register } from 'swiper/element/bundle'
-import { i18n } from './components/utils/i18n'
-import ToastPlugin from 'vue-toast-notification'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { MotionPlugin } from "@vueuse/motion";
+import Vue3Lottie from "vue3-lottie";
+import "./styles/globals.css";
+import { register } from "swiper/element/bundle";
+import { i18n } from "./components/utils/i18n";
+import ToastPlugin from "vue-toast-notification";
+import VueGtag, { config } from "vue-gtag";
 
-const app = createApp(App)
-library.add(fab, fas)
+const app = createApp(App);
+library.add(fab, fas);
 
-app.use(MotionPlugin)
-app.use(i18n)
-app.use(Vue3Lottie)
-app.use(ToastPlugin)
-app.component('FontAwesomeIcon', FontAwesomeIcon).mount('#app')
-register()
+app.use(MotionPlugin);
+app.use(i18n);
+app.use(Vue3Lottie);
+app.use(VueGtag, {
+	config: {
+		id: "G-B4DH8G4LNP",
+	},
+});
+app.use(ToastPlugin);
+app.component("FontAwesomeIcon", FontAwesomeIcon).mount("#app");
+register();
