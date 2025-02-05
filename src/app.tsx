d3671269a6f748478f9643242900af7e3c902ca0
Guilherme Fernandes <guilhermefernandessk8@hotmail.com>
2025-02-04 23:11:22 -0300

    feat: finish about section

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,14 @@
 import Header from "./components/Layout/header";
-<<<<<<< HEAD
-=======
 import About from "./components/Sections/about";
 import Stacks from "./components/Sections/stacks";
->>>>>>> a862d03 (feat: finish about section)
 
 const App = () => {
 	return (
 		<>
 			<Header />
-<<<<<<< HEAD
-			<main>
-				<p>oi</p>
-=======
 			<main className="flex flex-col items-center !mt-16">
 				<About />
 				<Stacks />
->>>>>>> a862d03 (feat: finish about section)
 			</main>
 		</>
 	);
